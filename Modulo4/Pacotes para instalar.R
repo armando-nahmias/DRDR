219431f3460d146efd9950f4e70bde4c8d4d0381
Armando Nahmias <76911464+armando-nahmias@users.noreply.github.com>
2022-05-31 13:33:40 -0400

    Modulo 4

---
+++ Modulo4/Pacotes para instalar.R
@@ -0,0 +1,6 @@
+pkgs <- c(
+  "dashboardthemes", "shiny", "shinydashboard", "shinyjs",
+  "data.table", "echarts4r", "highcharter", "leaflet", "plotly",
+  "tidyverse"
+)
+install.packages(pkgs, dependencies = TRUE)
